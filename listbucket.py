b9708c17f34f362e1008baa625674c49cab8651e
Anders Beitnes <beitnes@fearbanister-lm.corp.yahoo.com>
2017-06-22 22:35:20 -0700

    Add list handling, input validation, error handling,

--- listbucket.py
+++ listbucket.py
@@ -1,18 +1,26 @@
 #! /usr/bin/python
 import boto3
+import os
 import sys
 
 
 def get_bucket_list(account):
+
+    account = account.zfill(12)
+
     sts_client = boto3.client('sts')
 
     # Call the assume_role method of the STSConnection object and pass the role
     # ARN and a role session name.
-    assumed_role = sts_client.assume_role(
-        RoleArn="arn:aws:iam::" + account + ":role/SecurityMonkey",
-        RoleSessionName="TestAssumeRole"
-    )
-
+    try:
+        assumed_role = sts_client.assume_role(
+            RoleArn="arn:aws:iam::" + account + ":role/SecurityMonkey",
+            RoleSessionName="TestAssumeRole"
+        )
+    #  TODO Add pythonistic exception handling
+    except:
+        print("<<<<" + account + "AssumeRole error>>>>")
+        return
     # Get temporary credentials
     credentials = assumed_role['Credentials']
 
@@ -23,16 +31,29 @@ def get_bucket_list(account):
         aws_secret_access_key=credentials['SecretAccessKey'],
         aws_session_token=credentials['SessionToken'],
     )
+    print("\nAccount: {}".format(account))
 
     # Buckets Ahoy?
     for bucket in s3_resource.buckets.all():
-        print(bucket.name)
+        print("\t{}".format(bucket.name))
 
+def get_account_list(filename):
+    accounts = list()
+    with open(filename) as monitored_file:
+        for account in list(monitored_file):
+            account = account.strip()
+            if account.isdecimal():
+                accounts.append(account)
+    return accounts
 
 def main():
-    if len(sys.argv) == 3:
-        print(sys.argv[1])
-        get_bucket_list(sys.argv[1])
+    print (len(sys.argv))
+    if len(sys.argv) == 2:
+        if os.path.isfile(sys.argv[1]):
+            for account in get_account_list(sys.argv[1]):
+                get_bucket_list(account)
+        else:
+            get_bucket_list(sys.argv[1])
 
 
 print(__name__)
